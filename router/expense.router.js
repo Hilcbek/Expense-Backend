b424e0a0eb1dd9823bea2f9bbd28634860273afb
Bereket Alemayehu <86413773+Hilcbek@users.noreply.github.com>
2023-11-10 21:32:04 +0300

    Update expense.router.js

--- router/expense.router.js
+++ router/expense.router.js
@@ -2,8 +2,8 @@ import express from 'express'
 import { AddExpense, DeleteExpense, EditExpense, GetAllExpense, SingleExpense} from '../controller/expense.controller.js';
 import { verifyToken } from '../JWT/verifyToken.js';
 export let routerExpense = express.Router();
-routerExpense.post('/',verifyToken,AddExpense)
-routerExpense.get('/', verifyToken, GetAllExpense)
-routerExpense.get('/:id', verifyToken, SingleExpense)
-routerExpense.put('/edit/:id',verifyToken, EditExpense)
-routerExpense.delete('/delete/:id',verifyToken, DeleteExpense)
+routerExpense.post('/',AddExpense)
+routerExpense.get('/', GetAllExpense)
+routerExpense.get('/:id', SingleExpense)
+routerExpense.put('/edit/:id', EditExpense)
+routerExpense.delete('/delete/:id', DeleteExpense)
